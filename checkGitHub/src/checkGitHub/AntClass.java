65b44d5d9a18b985044df2557ff7e24e5b602e2b
Owner <Owner@Andy.gm.com>
2015-12-06 20:18:50 -0500

    simple ant class

---
+++ checkGitHub/src/checkGitHub/AntClass.java
@@ -0,0 +1,14 @@
+package checkGitHub;
+
+import org.testng.annotations.Test;
+
+public class AntClass {
+	
+	@Test
+	
+	public static void simpleFunc(){
+		
+		System.out.println("Welcome to Build.xml ANT world");
+	}
+
+}
